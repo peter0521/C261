87bace22d31e2e38c952b623aa15e67e4744e441
peter0521 <peter0521@gmail.com>
2015-10-30 19:36:09 +0800

    輸入Label名稱

--- Example02/Form1.Designer.cs
+++ Example02/Form1.Designer.cs
@@ -124,18 +124,18 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(109, 244);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.Size = new System.Drawing.Size(86, 18);
             this.label1.TabIndex = 8;
-            this.label1.Text = "label1";
+            this.label1.Text = "label1名稱";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(427, 244);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 18);
+            this.label2.Size = new System.Drawing.Size(86, 18);
             this.label2.TabIndex = 9;
-            this.label2.Text = "label2";
+            this.label2.Text = "label2名稱";
             // 
             // Form1
             // 
