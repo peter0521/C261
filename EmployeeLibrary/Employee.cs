5058c60e352e24ce7c09aed2cee8f3e2a3abb4fd
peter0521 <peter0521@gmail.com>
2015-10-30 20:12:09 +0800

    新增 Employee 類別

---
+++ EmployeeLibrary/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLibrary
+{
+    public class Employee
+    {
+        private string firdtName;
+        private string secondName;
+        private int baseSalary;
+
+        private int benifity;
+        private int salary;
+
+        public string FirstName
+        {
+            get { return this.firdtName; }
+            set { this.firdtName = value; }
+        }
+
+        public string SecondName
+        {
+            get { return secondName; }
+
+            set { secondName = value; }
+        }
+
+        public int BaseSalary
+        {
+            get { return baseSalary; }
+            set { baseSalary = value; }
+        }
+
+        public int Benifity
+        {
+            get { return benifity; }
+            set { benifity = value; }
+        }
+
+        public int Salary
+        {
+            get { return Benifity + BaseSalary; }
+        }
+    }
+}
